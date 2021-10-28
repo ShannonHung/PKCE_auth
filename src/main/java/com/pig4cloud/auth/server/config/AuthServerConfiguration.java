dfd32a56cd138dea275b96596ae26f9209060530
Shannonhung <b10709036@gapps.ntust.edu.tw>
2021-10-28 16:22:02 +0800

    first commit

---
+++ src/main/java/com/pig4cloud/auth/server/config/AuthServerConfiguration.java
@@ -0,0 +1,161 @@
+package com.pig4cloud.auth.server.config;
+
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.JWSKeySelector;
+import com.nimbusds.jose.proc.JWSVerificationKeySelector;
+import com.nimbusds.jose.proc.SecurityContext;
+import com.nimbusds.jwt.proc.ConfigurableJWTProcessor;
+import com.nimbusds.jwt.proc.DefaultJWTProcessor;
+import lombok.SneakyThrows;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.MediaType;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.authorization.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.OAuth2Error;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.ClientSettings;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import java.nio.charset.StandardCharsets;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+/**
+ * @author lengleng
+ * @date 2021/2/15
+ */
+@Configuration
+@EnableWebSecurity
+public class AuthServerConfiguration {
+    private static final Logger log = LoggerFactory.getLogger(AuthServerConfiguration.class);
+
+	@Bean
+	@Order(Ordered.HIGHEST_PRECEDENCE)
+	public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
+
+
+        //OAuth2AuthorizationServerConfiguration.java裡面的
+        OAuth2AuthorizationServerConfigurer<HttpSecurity> authorizationServerConfigurer =
+                new OAuth2AuthorizationServerConfigurer<>();
+        RequestMatcher endpointsMatcher = authorizationServerConfigurer.getEndpointsMatcher();
+
+        //authorization 端點設定
+        authorizationServerConfigurer
+                .authorizationEndpoint(config ->
+                        config.errorResponseHandler((req, res, ex) -> {
+                            OAuth2AuthenticationException oAuth2AuthenticationException  = (OAuth2AuthenticationException) ex;
+                            OAuth2Error error = oAuth2AuthenticationException.getError();
+                            log.error("錯誤原因:[{}]", error);
+                            log.info("認證異常", ex);
+                            res.setCharacterEncoding(StandardCharsets.UTF_8.name());
+                            res.setContentType(MediaType.APPLICATION_JSON.toString());
+                            res.getWriter().write("{\"code\":-1,\"msg\":\"认证失败\"}");
+                        })
+                );
+
+        //token 端點客製化設定
+        authorizationServerConfigurer
+                .tokenEndpoint(config ->
+                        config.errorResponseHandler((req, res, ex) -> {
+                            OAuth2AuthenticationException oAuth2AuthenticationException  = (OAuth2AuthenticationException) ex;
+                            OAuth2Error error = oAuth2AuthenticationException.getError();
+                            log.error("錯誤原因:[{}]", error);
+                            log.info("認證異常", ex);
+                            res.setCharacterEncoding(StandardCharsets.UTF_8.name());
+                            res.setContentType(MediaType.APPLICATION_JSON.toString());
+                            res.getWriter().write("{\"code\":-1,\"msg\":\"认证失败\"}");
+                        })
+                );
+
+
+        //OAuth2AuthorizationServerConfiguration.java裡面的
+        http
+                .requestMatcher(endpointsMatcher)
+                .authorizeRequests(authorizeRequests -> authorizeRequests
+                        .anyRequest().authenticated()
+                )
+                .csrf(csrf -> csrf.ignoringRequestMatchers(endpointsMatcher))
+                .apply(authorizationServerConfigurer)
+                .and().formLogin(Customizer.withDefaults());
+
+        return http.build();
+	}
+
+	// @formatter:off
+    @Bean
+    public RegisteredClientRepository registeredClientRepository() {
+        RegisteredClient client = RegisteredClient.withId("pig")
+                .clientId("pig")
+                .authorizationGrantTypes(authorizationGrantTypes -> {
+                    authorizationGrantTypes.add(AuthorizationGrantType.AUTHORIZATION_CODE);
+                    authorizationGrantTypes.add(AuthorizationGrantType.REFRESH_TOKEN);
+                })
+                .scope("read")
+                .redirectUri("http://localhost:9090/authorized")
+                .clientSettings(ClientSettings.builder().requireProofKey(true).requireAuthorizationConsent(true).build())
+                .build();
+        return new InMemoryRegisteredClientRepository(client);
+    }
+    // @formatter:on
+
+
+	@Bean
+	@SneakyThrows
+	public JWKSource<SecurityContext> jwkSource() {
+		KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+		keyPairGenerator.initialize(2048);
+		KeyPair keyPair = keyPairGenerator.generateKeyPair();
+		RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+		RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+
+		// @formatter:off
+        RSAKey rsaKey= new RSAKey.Builder(publicKey)
+                .privateKey(privateKey)
+                .keyID(UUID.randomUUID().toString())
+                .build();
+        JWKSet jwkSet = new JWKSet(rsaKey);
+        return (jwkSelector, securityContext) -> jwkSelector.select(jwkSet);
+    }
+
+    @Bean
+    public static JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource) {
+        Set<JWSAlgorithm> jwsAlgs = new HashSet<>();
+        jwsAlgs.addAll(JWSAlgorithm.Family.RSA);
+        jwsAlgs.addAll(JWSAlgorithm.Family.EC);
+        jwsAlgs.addAll(JWSAlgorithm.Family.HMAC_SHA);
+        ConfigurableJWTProcessor<SecurityContext> jwtProcessor = new DefaultJWTProcessor<>();
+        JWSKeySelector<SecurityContext> jwsKeySelector =
+                new JWSVerificationKeySelector<>(jwsAlgs, jwkSource);
+        jwtProcessor.setJWSKeySelector(jwsKeySelector);
+        // Override the default Nimbus claims set verifier as NimbusJwtDecoder handles it instead
+        jwtProcessor.setJWTClaimsSetVerifier((claims, context) -> {
+        });
+        return new NimbusJwtDecoder(jwtProcessor);
+    }
+
+
+}
